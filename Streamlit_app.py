2543a79cb2dd983a41ecdae9fc91d6e6177d3608
Awtum <awtum@usf.edu>
2022-02-28 21:42:41 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,13 +11,21 @@ st.title('classification of rare diseases')
 
 url="https://raw.githubusercontent.com/Awtum/Topic3_TeamA/main/OrphaICD10.tsv"
 
-st.selectbox("orpha code", options=('1','2','3','4','5'))
+code=st.selectbox("orpha code", options=('1','2','3','4','5'))
+
+st.write('You selected:', code)
 
 def load_data(nrows):
     data=pd.read_csv(url, nrows=nrows, sep='\t')
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
+
+
+#col1, col2, col3 = st.columns(3)
+#col1.metric("Description", "1.2 °F")
+#col2.metric("ICD-10", "-8%")
+#col3.metric("Orpha", "4%")
 
 
 data_load_state = st.text('Loading data...')
