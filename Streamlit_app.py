744148c00346ff45f7c02bc969753d7b903cd041
Awtum <awtum@usf.edu>
2022-02-28 20:45:15 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('classification of rare diseases')
+
+
+def load_data(nrows):
+    data=pd.read_table("github.com/awtum/Topic3_TeamA/Streamlit_app.py", nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data(100)
+data
