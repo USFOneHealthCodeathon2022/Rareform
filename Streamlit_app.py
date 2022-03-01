a9cff73100df9e0ea9aa54843387967c94796dcb
Awtum <awtum@usf.edu>
2022-02-28 21:03:14 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,13 +1,13 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 st.title('classification of rare diseases')
 
+url="https://raw.githubusercontent.com/Awtum/Topic3_TeamA/main/OrphaICD10.tsv"
 
 def load_data(nrows):
-    data=pd.read_csv("github.com/awtum/Topic3_TeamA/OrphaICD10.tsv", nrows=nrows, sep='\t')
+    data=pd.read_csv(url, nrows=nrows, sep='\t')
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
